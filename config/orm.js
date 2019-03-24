b3bac0c6970eca7e66cbdb1ae702e51ac3fe33d7
Nate Micinski <nmicinski@gmail.com>
2019-03-23 17:16:20 -0700

    findall now accepts different table inputs

--- config/orm.js
+++ config/orm.js
@@ -10,14 +10,13 @@ var connection = require("./connection.js");
 // Define query methods
 var orm = {
     selectAll: function(table, cb) {
-        var queryString = "SELECT * FROM burgers";
-        connection.query(queryString, function(err, result) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [table], function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
     insertOne: function() {
-
     },
     updateOne: function() {
 
