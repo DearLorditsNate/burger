69719b80cbfeba3901e0887826c3e0fc7a498902
Nate Micinski <nmicinski@gmail.com>
2019-03-24 19:27:17 -0700

    update connection to JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -11,13 +11,19 @@ require("dotenv").config();
 var keys = require("../keys.js");
 
 // Define connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: keys.mysql.password,
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: keys.mysql.password,
+        database: "burgers_db"
+    });
+}
 
 // Create connection
 connection.connect(function (err) {
