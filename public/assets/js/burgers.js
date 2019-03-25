1fe42df15c925c62beb25723a4f8536b48465ccb
Nate Micinski <nmicinski@gmail.com>
2019-03-24 19:08:47 -0700

    update burger now works

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,31 @@
 console.log("testing!");
+
+$(document).ready(function() {
+    // Add burger
+    $(".add-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#burger").val()
+        }
+        console.log(newBurger);
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function() {
+            location.reload();
+        });
+    });
+
+    // Update burger
+    $(".update").on("click", function() {
+        var id = $(this).data("id");
+        
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT"
+        }).then(function() {
+            location.reload();
+        });
+    });
+});
